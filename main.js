82fb9668ba37df9a83ad638042e44bfd18f40be0
Josh Brzinski <jbbrzinski@gmail.com>
2015-09-30 00:10:05 -0400

    ok so the code WORKS, but I don't know if I need window.onload or what, and then I'm not sure how to get it to execute everything in order AFTER I click calculate.  FML

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 //Input fields + turning strings into numbers 
-var input1 = document.querySelector('#num_one').replace ( /[^\d.]/g, '' )
-var input2 = document.querySelector('#num_two').replace ( /[^\d.]/g, '' )
+var input1 = document.querySelector('#num_one')
+var in1num = input1.value .replace ( /[^\d.]/g, '' )
+var input2 = document.querySelector('#num_two')
+var in2num = input2.value .replace ( /[^\d.]/g, '' )
 
 //The Button!
 var calculate = document.querySelector('#calculateBtn')
-calculate.addEventListener('click',domath(input1,input2))
+calculate.addEventListener('click',domath(in1num,in2num))
 
 //Answer and calculation func
 var answer = document.querySelector('#answer')
@@ -12,5 +14,6 @@ var answer = document.querySelector('#answer')
 var domath = function(x,y) {
   answer.value = x + y
   console.log(answer.value)
-  alert
+  answer.innerHTML = x + y
+  console.log(answer.innerHTML)
 }
