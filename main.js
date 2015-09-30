2c2d9c663290716c7da18cf2637e543c24795f3f
Josh Brzinski <jbbrzinski@gmail.com>
2015-09-29 23:52:05 -0400

    Truly, fuck DOM.  I don't even know if the damn thing is working.

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 var input1 = document.querySelector('#num_one').replace ( /[^\d.]/g, '' )
 var input2 = document.querySelector('#num_two').replace ( /[^\d.]/g, '' )
 
-var answer = document.querySelector('#answer')
-
-var domath() = function(x,y) {
-  answer.value = x + y
-  console.log(answer.value)
-}
-
 //The Button!
 var calculate = document.querySelector('#calculateBtn')
 calculate.addEventListener('click',domath(input1,input2))
+
+//Answer and calculation func
+var answer = document.querySelector('#answer')
+
+var domath = function(x,y) {
+  answer.value = x + y
+  console.log(answer.value)
+  alert
+}
