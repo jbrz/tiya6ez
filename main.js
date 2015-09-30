a6e3b5bef6d8e12c0d8039448ad9fe44af2c57e0
Josh Brzinski <jbbrzinski@gmail.com>
2015-09-30 09:23:25 -0400

    dumb.

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-//Input fields + turning strings into numbers 
+//Input fields
 var input1 = document.querySelector('#num_one')
-var in1num = input1.value .replace ( /[^\d.]/g, '' )
 var input2 = document.querySelector('#num_two')
-var in2num = input2.value .replace ( /[^\d.]/g, '' )
-
-//The Button!
-var calculate = document.querySelector('#calculateBtn')
-calculate.addEventListener('click',domath(in1num,in2num))
-
-//Answer and calculation func
 var answer = document.querySelector('#answer')
+var calculate = document.querySelector('#calculateBtn')
 
-var domath = function(x,y) {
-  answer.value = x + y
+
+//The Button! And its function!
+calculate.addEventListener('click',function(x,y) {
+  var in1num = parseInt(input1.value, 10)
+  var in2num = parseInt(input2.value, 10);
+  
+  var result = in1num + in2num
+  answer.value = result
   console.log(answer.value)
-  answer.innerHTML = x + y
-  console.log(answer.innerHTML)
-}
+  answer.textContent = result
+})
+
+/* yep, still not showing on screen.  solid.  fml. */
